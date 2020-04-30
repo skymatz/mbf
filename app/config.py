331affce30487457b983f97f3c0723c092f325ab
@SKYMATZ <63637812+skymatz@users.noreply.github.com>
2020-04-30 15:06:30 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,8 +5,8 @@
 # Name           : Multi BF (MBF) <cookie method>     #
 # File           : config.py                          #
 # Author         : DulLah                             #
-# Recoder        : SkyMatz                             #
-# Github         : https://github.com/skymatz           #
+# Recoder        : SkyMatz                            #
+# Github         : https://github.com/skymatz         #
 # Facebook       : https://www.facebook.com/dulahz    #
 # Telegram       : https://t.me/unikers               #
 # Python version : 2.7                                #
@@ -26,28 +26,20 @@ class Config:
 
 	def banner(self):
 		return '''\n
+    }--------------------------------------------{
+}----------> Multi Brute Force Cookies Method <----------{
+    }--------------------------------------------{ v 1.0
 
-\033[0;96m    __  _____    ____   ______            __   _          
-   /  |/  / /_  / __/  / ____/___  ____  / /__(_)__  _____
-  / /|_/ / __ \/ /_   / /   / __ \/ __ \/ //_/ / _ \/ ___/
- / /  / / /_/ / __/  / /___/ /_/ / /_/ / ,< / /  __(__  ) 
-/_/  /_/_.___/_/     \____/\____/\____/_/|_/_/\___/____/ \033[33;6mv1.0 \033[0m
-
-\033[0;32m[+]=====================================================[+]
-[+] Author   : SkyMatz                                  [+]
-[+] Github   : https://github.com/skymatz               [+]
-[+] Facebook : https://facebook.com/amtpags             [+]
-[+]=====================================================[+]
-\033[0m '''
-
+  coded by skymatz | fb.me/amttidz | github.com/skymatz
+'''
 	def httpRequest(self, url, cookies):
 		try:
 			return requests.get(url, cookies = {'cookie': cookies}).text
 		except requests.exceptions.RequestException:
-			exit('\n\n\033[0;91mConnection error, check your connection!!\033[0m')
+			exit('\n\n\033[0;91mKoneksi error coek;v\033[0m')
 
 	def httpRequestPost(self, url, cookies, params):
 		try:
 			return requests.post(url, data = params, cookies = {'cookie': cookies}).text
 		except requests.exceptions.RequestException:
-			exit('\n\n\033[0;91mConnection error, check your connection!!\033[0m')
+			exit('\n\n\033[0;91mKoneksi error coek;v\033[0m')
