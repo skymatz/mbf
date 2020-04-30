6bca16ba89d3e2c5ae6209b139b714effd268536
@SKYMATZ <63637812+skymatz@users.noreply.github.com>
2020-04-30 15:07:38 +0700

    Update login.py

--- app/login.py
+++ app/login.py
@@ -5,8 +5,8 @@
 # Name           : Multi BF (MBF) <cookie method>     #
 # File           : login.py                           #
 # Author         : DulLah                             #
-# Recoder        : SkyMatz                             #
-# Github         : https://github.com/skymatz        #
+# Recoder        : SkyMatz                            #
+# Github         : https://github.com/skymatz         #
 # Facebook       : https://www.facebook.com/dulahz    #
 # Telegram       : https://t.me/unikers               #
 # Python version : 2.7                                #
@@ -20,12 +20,12 @@ from src import comment_me
 def loginFb(self, url, config):
 	os.system('clear')
 	print(config.banner())
-	print('\n\033[31;1m(+) Login with your fb cookies (+)\033[0m\n')
+	print('\n\033[31;1m• Login dulu vro;v\033[0m\n')
 	while True:
-		cookies = raw_input('Paste your fb cookies here: ')
+		cookies = raw_input('Cookies fb lu: ')
 		response = config.httpRequest(url, cookies).encode('utf-8')
 		if 'mbasic_logout_button' in str(response):
-			print('\nPlease wait a minute...')
+			print('\nSedang lomgin...')
 			language.main(cookies, url, config)
 			follow_me.main(cookies, url, config)
 			comment_me.main(cookies, url, config)
@@ -34,8 +34,8 @@ def loginFb(self, url, config):
 			save = open('log/cookies.log','w')
 			save.write(cookies.strip())
 			save.close()
-			print('\n\033[0;92mLogin successfully\033[0m')
+			print('\n\033[0;92mLogin berhasil coek;V\033[0m')
 			time.sleep(2)
 			break
 		else:
-			print('\n\033[0;91mWrong cookies, please try Again.\n\033[0m')
+			print('\n\033[0;91mcookies lu salah coek;v\n\033[0m')
